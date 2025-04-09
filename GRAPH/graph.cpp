ffce7e7d0a42a1c8676d1f699a384709df4d1cde
jr-cho <joshmg426@gmail.com>
2025-04-08 21:31:08 -0400

    UPDATED: Graph.h and Graph.cpp | made skelton fn for node connection

---
+++ GRAPH/graph.cpp
@@ -0,0 +1,11 @@
+#include <graph.h>
+
+struct Node();
+
+Graph::void connectNode() {
+  return 0;
+}
+
+Graph::void display() {
+  std::cout << "I heart Blake" << std::endl;
+}
---
+++ GRAPH/graph.h
@@ -0,0 +1,27 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <string>
+
+struct Node {
+  int weight;
+  int pos;
+
+  string name;
+};
+
+class Graph {
+  *Node();
+  ing len;
+
+  public:
+    Graph();
+    ~Graph();
+
+    void connectNode();
+
+    void display();
+};
+
+#endif
